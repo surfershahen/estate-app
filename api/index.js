68edc98ca8778efc56228855b51843c3f70bc798
surfer971 <57986206+surfer971@users.noreply.github.com>
2024-05-20 10:26:51 +0300

    "create a middleware and a function to handle possible errors"

--- api/index.js
+++ api/index.js
@@ -19,9 +19,20 @@ mongoose
 const app = express();
 app.use(express.json());
 
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
