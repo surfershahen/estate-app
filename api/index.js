9fdae40f135335c271604ceb7f595de52634b651
surfer971 <57986206+surfer971@users.noreply.github.com>
2024-06-23 02:59:27 +0300

    "update index.js & add static files"

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -17,6 +18,7 @@ mongoose
   .catch(err => {
     console.log(err);
   });
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -29,6 +31,10 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
